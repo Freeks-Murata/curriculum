37702fb9abf81986be3c2e44457c3c7bbbddf47e
Haya16-2450 <90101067+Freeks-Murata@users.noreply.github.com>
2021-09-05 19:27:51 +0900

    課題提出テスト
    データ型・文字列クラス(java)の作成

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,23 @@
+public class Task1_1{
+    public static void main(String[] args){
+
+        /* int hogeInt=50; */
+
+        /* String hogeString="テスト"; */
+
+        /* doubleクラスのhogeDouble"という名前を宣言し、"0.2"という数値で初期化させている。*/
+        /* また、intでは小数点以下の数字を扱うことができないためdoubleを使用している。*/
+        double hogeDouble = 0.2;
+
+        int hogeInt=100;
+
+        String hogeString="合格";
+
+        System.out.println(hogeInt);
+
+        /* 上記で初期化した変数hogeStringの値"合格"を表示させようとしている。 */
+        System.out.println(hogeString);
+
+        System.out.print(hogeDouble);
+    }
+}
