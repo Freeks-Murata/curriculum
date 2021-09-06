fafdb71eddd0d16dd694271bea0f90b967670749
Haya16-2450 <90101067+Freeks-Murata@users.noreply.github.com>
2021-09-07 00:32:28 +0900

    データ型・文字列クラス 課題提出テスト
    Task1_1.javaの修正

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -12,10 +12,10 @@ public class Task1_1 {
     public static void main(String[] args) {
 
         // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
-        // int hogeInt=50;
+        int hogeInt=50;
 
         // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
-        // String hogeString="テスト"; 
+        String hogeString="テスト"; 
 
         // ③下記の処理について、何をしているのかコメントを記入してください
         /* doubleクラスのhogeDouble"という名前を宣言し、"0.2"という数値で初期化させている。
@@ -23,10 +23,10 @@ public class Task1_1 {
         double hogeDouble = 0.2;
 
         // ④変数 hogeInt を値「100」で上書き
-        int hogeInt=100;
+        hogeInt=100;
 
         // ⑤変数 hogeString を値「合格」で上書き
-        String hogeString="合格";
+        hogeString="合格";
 
         // ⑥変数 hogeInt の値を表示
         System.out.println(hogeInt);
