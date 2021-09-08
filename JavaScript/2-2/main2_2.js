3fd1f4b6c72bac99debebed8046d2e8415f20493
Haya16-2450 <90101067+Freeks-Murata@users.noreply.github.com>
2021-09-08 16:26:36 +0900

    要素の取得 document.querySelector... 課題提出テスト.
    index2_2.html
    main2_2.jsの作成

---
+++ JavaScript/2-2/main2_2.js
@@ -0,0 +1,9 @@
+document.getElementById("p1").p1.style.display = "none";
+
+function changeBtn() {
+    if (document.getElementById("p1").style.display === "block") {
+        document.getElementById("p1").style.display = "none";
+    } else {
+        document.getElementById("p1").style.display = "block";
+    }
+}
