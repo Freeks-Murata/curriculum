6cf232537c3d9d6809e1ae057282bdb860ea4399
Haya16-2450 <90101067+Freeks-Murata@users.noreply.github.com>
2021-09-12 18:47:24 +0900

    2章チェックテスト 課題提出
    index2_5.html
    main2_5.jsの作成

---
+++ JavaScript/2-5/main2_5.js
@@ -0,0 +1,15 @@
+function changeLang() {
+        let japan = document.getElementById("japan");
+        let usa = document.getElementById("usa");
+        let options = document.querySelectorAll("option");
+        if (options[1].selected) {
+                japan.style.display = "block";
+                usa.style.display = "none";
+        } else if (options[2].selected) {
+                japan.style.display = "none";
+                usa.style.display = "block";
+        } else if (options[3].selected) {
+                japan.style.display="block";
+                usa.style.display="block";
+        }
+}
